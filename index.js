60256c5092a222d5615606550d4667fa131f9fdd
Ridho Prasetya <ndiing.inc@gmail.com>
2022-08-16 13:48:29 +0700

    save

--- index.js
+++ index.js
@@ -1,251 +1,133 @@
-const Crypto = require("@ndiing/crypto");
-const { URL2 } = require("@ndiing/fetch");
+const Crypto = require("@ndiinginc/crypto");
+const { URL2 } = require("@ndiinginc/fetch");
 
-/**
- * ### Install
- * ```
- * npm install @ndiing/otp
- * ```
- * ### Usage
- * ```js
- * var options = {
- *     type: "totp",
- *     label: "ndiing",
- *     // secret,
- *     encoding: "base32",
- *     issuer: "ndiing.inc@google.com",
- *     algorithm: "sha1",
- *     digits: 6,
- *     // counter,
- *     period: 30,
- * };
- * var token = OTP.generate(options);
- * console.log({token})
- *
- * var valid = OTP.validate(token,options);
- * console.log({valid})
- *
- * var secret = OTP.generateSecret();
- * console.log({secret})
- *
- * var url = OTP.generateOtpauth(options);
- * console.log({url})
- * ```
- * @module otp
- */
+class Signer {
+    // https://www.rfc-editor.org/rfc/rfc4226.html
 
-/**
- *
- */
-class Generator {
-    /**
-     * HOTP: An HMAC-Based One-Time Password Algorithm
-     * @see {@link https://www.rfc-editor.org/rfc/rfc4226}
-     * @param {Object} options
-     * @returns {String}
-     */
     static hotp(options = {}) {
-        let { secret, encoding = "ascii", counter = 0, algorithm = "sha1", digits = 6 } = options;
-
+        let { encoding = "ascii", secret = "", algorithm = "sha1", digits = 6, counter = 0 } = options;
         if (encoding == "base32") {
             secret = Crypto.base32Decode(secret);
         } else {
             secret = Buffer.from(secret, encoding);
         }
 
-        const count = Buffer.alloc(8);
-        count.writeUInt32BE(counter, 4);
+        const data = Buffer.alloc(8);
+        data.writeUInt32BE(counter, 4);
 
-        const hmac = Crypto.hmac(count, { algorithm, key: secret, encoding: "hex" });
+        const hmac = Crypto.hmac(data, { algorithm, key: secret });
 
         const offset = parseInt(hmac.charAt(hmac.length - 1), 16);
 
         let result = parseInt(hmac.substr(offset * 2, 2 * 4), 16);
         result = result & 0x7fffffff;
-        result = ("" + result).padStart(digits, "0");
+        result = String(result).padStart(digits, "0");
+        result = result.slice(0 - digits);
 
-        // otp
-        return result.slice(0 - digits);
+        return result;
     }
 
-    /**
-     * TOTP: Time-Based One-Time Password Algorithm
-     * @see {@link https://www.rfc-editor.org/rfc/rfc6238}
-     * @param {Object} options
-     * @returns {String}
-     */
+    // https://www.rfc-editor.org/rfc/rfc6238.html
+
     static totp(options = {}) {
-        let { secret = "", encoding = "ascii", time = Date.now(), epoch = 0, period = 30, algorithm = "sha1", digits = 6 } = options;
+        let { time = Date.now(), epoch = 0, period = 30 } = options;
         time = time / 1000;
-
-        const counter = Math.floor((time - epoch) / period);
-
-        return this.hotp({ secret, counter, algorithm, digits, encoding });
+        options.counter = Math.floor((time - epoch) / period);
+        return this.hotp(options);
     }
 }
 
-/**
- *
- */
-class Validator {
-    /**
-     *
-     * @param {String} token
-     * @param {Object} options
-     * @returns {Boolean}
-     */
-    static hotp(token, options) {
-        return token == Generator.hotp(options);
+// console.log(Signer.hotp({
+//     secret:'12345678901234567890'
+// }))
+
+class Verifier {
+    static hotp(data, options = {}) {
+        return data == Signer.hotp(options);
     }
 
-    /**
-     *
-     * @param {String} token
-     * @param {Object} options
-     * @returns {Boolean}
-     */
-    static totp(token, options) {
-        return token == Generator.totp(options);
+    static totp(data, options = {}) {
+        return data == Signer.totp(options);
     }
 }
 
-/**
- *
- */
 class OTP {
-    /**
-     *
-     * @param {Object} options
-     * @param {String} options.type -  `hotp`, or `totp`
-     * @param {String} options.secret
-     * @param {String} options.encoding=base32 - `base32`, `ascii`, `hex`
-     * @param {Number} options.counter=0 - used with `hotp`
-     * @param {String} options.algorithm=sha1 - `sha1`, `sha256`, or `sha512`
-     * @param {Number} options.digits=6
-     * @param {Number} options.time=Date.now()/1000 - use with `totp`
-     * @param {Number} options.epoch=0 - use with `totp`
-     * @param {Number} options.period=30 - use with `totp`
-     * @returns {String}
-     */
     static generate(options = {}) {
-        const { type = "hotp" } = options;
-        // type=hotp/totp
-        return Generator[type](options);
+        const { type } = options;
+        return Signer[type](options);
     }
 
-    /**
-     *
-     * @param {String} token
-     * @param {Object} options
-     * @param {String} options.type -  `hotp`, or `totp`
-     * @param {String} options.secret
-     * @param {String} options.encoding=base32 - `base32`, `ascii`, `hex`
-     * @param {Number} options.counter=0 - used with `hotp`
-     * @param {String} options.algorithm=sha1 - `sha1`, `sha256`, or `sha512`
-     * @param {Number} options.digits=6
-     * @param {Number} options.time=Date.now()/1000 - use with `totp`
-     * @param {Number} options.epoch=0 - use with `totp`
-     * @param {Number} options.period=30 - use with `totp`
-     * @returns {Boolean}
-     */
-    static validate(token, options = {}) {
-        const { type = "hotp" } = options;
-        // type=hotp/totp
-        return Validator[type](token, options);
+    static validate(data, options = {}) {
+        const { type } = options;
+        return Verifier[type](data, options);
     }
 
-    /**
-     *
-     * @param {Object} options
-     * @returns {String}
-     * @see {@link https://www.rfc-editor.org/rfc/rfc3548}
-     */
-    static generateSecret(options = {}) {
+    static secret(options = {}) {
         const { algorithm = "sha1", encoding = "base32" } = options;
-        const char =
-            "0123456789" + //
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-            "abcdefghijklmnopqrstuvwxyz";
-        const end = algorithm == "sha256" ? 32 : algorithm == "sha512" ? 65 : 20;
+        const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
         let string = "";
         while (string.length < 64) {
-            string += char[Math.floor(Math.random() * 62)];
+            const number = Math.floor(Math.random() * char.length);
+            string += char[number];
         }
-        string = encoding == "base32" ? Crypto.base32Encode(string) : Buffer.from(string).toString(encoding);
-        return string.slice(0, end);
+
+        return Buffer.alloc(algorithm == "sha256" ? 32 : algorithm == "sha512" ? 64 : 20)
+            .fill(encoding == "base32" ? Crypto.base32Encode(string) : Buffer.from(string).toString(encoding))
+            .toString();
     }
 
-    /**
-     *
-     * @param {Object} options
-     * @property {String} type=totp
-     * @property {String} label=label
-     * @property {String} secret=<AUTO>
-     * @property {String} encoding=base32
-     * @property {String} issuer=issuer
-     * @property {String} algorithm=sha1
-     * @property {String} digits=6
-     * @property {String} counter
-     * @property {String} period=30
-     * @returns {Object}
-     * @see {@link https://github.com/google/google-authenticator/wiki/Key-Uri-Format}
-     */
-    static generateOtpauth(options = {}) {
-        let {
-            //
-            type = "totp",
-            label = "label",
-            secret,
-            encoding = "base32",
-            issuer = "issuer",
-            algorithm = "sha1",
-            digits = 6,
-            counter,
-            period = 30,
-        } = options;
-        secret = secret || this.generateSecret({ algorithm, encoding });
+    // https://github.com/google/google-authenticator/wiki/Key-Uri-Format
 
-        let url = new URL2(`otpauth://${type}/${label}`);
+    static authenticator(options = {}) {
+        let { type = "totp", label = "label", secret = "", encoding = "base32", issuer = "", algorithm = "sha1", digits = 6, counter = 0, period = 30 } = options;
+        let url = new URL2(`otpauth://${type}/${label}?PARAMETERS`);
+        secret = secret || this.secret({ algorithm, encoding });
         if (secret) url.searchParams.set("secret", secret);
         if (issuer) url.searchParams.set("issuer", encodeURIComponent(issuer));
-        if (algorithm) url.searchParams.set("algorithm", algorithm.toUpperCase());
+        if (algorithm) url.searchParams.set("algorithm", algorithm);
         if (digits) url.searchParams.set("digits", digits);
-        if (counter && !type == "totp") url.searchParams.set("counter", counter);
-        if (period && !type == "hotp") url.searchParams.set("period", period);
+        if (type == "hotp" && counter !== undefined) url.searchParams.set("counter", counter);
+        if (type == "totp" && period) url.searchParams.set("period", period);
         url = "" + url;
 
-        let url2 = new URL2(`https://www.google.com/chart?chs=200x200&chld=M|0&cht=qr`);
-        url2.searchParams.set("chl", url);
-        url2 = "" + url2;
-
-        return url2;
+        let qr = new URL2("https://www.google.com/chart?chs=256x256&chld=M|0&cht=qr");
+        // &chl=
+        qr.searchParams.set("chl", url);
+        qr = "" + qr;
+        return qr;
     }
 }
 
-OTP.Generator = Generator;
-OTP.Validator = Validator;
 module.exports = OTP;
 
-// // ### Usage
-// var options = {
+// const OTP = require('@ndiinginc/otp')
+
+// // Generate random secret
+// console.log(OTP.secret({encoding:'base32'}));
+// console.log(OTP.secret({encoding:'base64'}));
+// console.log(OTP.secret({encoding:'hex'}));
+// console.log(OTP.secret({encoding:'ascii'}));
+// console.log(OTP.secret({encoding:'base32', algorithm:'sha256'}));
+// console.log(OTP.secret({encoding:'base64', algorithm:'sha256'}));
+// console.log(OTP.secret({encoding:'hex', algorithm:'sha256'}));
+// console.log(OTP.secret({encoding:'ascii', algorithm:'sha256'}));
+
+// // Generate google authenticator
+// console.log(OTP.authenticator());
+
+// // Generate token
+// console.log(OTP.generate({
 //     type: "totp",
-//     label: "ndiing",
-//     // secret,
+//     secret: "MVEFKULLOE4HURTBIFNH",
 //     encoding: "base32",
-//     issuer: "ndiing.inc@google.com",
 //     algorithm: "sha1",
-//     digits: 6,
-//     // counter,
-//     period: 30,
-// };
-// var token = OTP.generate(options);
-// console.log({token})
+// }));
 
-// var valid = OTP.validate(token,options);
-// console.log({valid})
-
-// var secret = OTP.generateSecret();
-// console.log({secret})
-
-// var url = OTP.generateOtpauth(options);
-// console.log({url})
+// // Validate token
+// console.log(OTP.validate('745237',{
+//     type: "totp",
+//     secret: "MVEFKULLOE4HURTBIFNH",
+//     encoding: "base32",
+//     algorithm: "sha1",
+// }));
